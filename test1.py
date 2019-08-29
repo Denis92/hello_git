9d8accae6c75fd0eeb6c336d7a41f79451ba1c31
denis <Denis.Gorshkov76@gmail.com>
2019-08-29 19:36:52 +0300

    Исправление ошибки

--- test1.py
+++ test1.py
@@ -1,3 +1,9 @@
 list_number = [i for i in range(10)]
 print(list_number)
 print("Hello world")
+list_number = [i for i in range(10)]
+print(list_number)
+print("Hello world")
+list_number = [i for i in range(10)]
+print(list_number)
+print("Hello world")
